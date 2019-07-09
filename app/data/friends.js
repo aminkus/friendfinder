adeb4b1dd59774cde9dea752b66370d36ad8272e
Adam Minkus <adam.minkus@gmail..com>
2019-07-08 17:12:13 -0700

    built html, routing, initialized data

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friendsArray = [
+    {
+        name: "Adam",
+        photo: "#",
+        scores: [5, 1, 2, 4, 3, 5, 2, 3, 3, 5]
+    }
+]
+
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var express = require("express");
 var app = express();
 //structure to support Heroku dynamic port
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
-app.get("/", function(req,res) {
-    res.send('hello adam')
-});
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 
 app.listen(PORT, function() {
     console.log(`listening on ${PORT}`)
