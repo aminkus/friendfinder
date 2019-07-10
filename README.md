e9e131462e9b1b2f57764224e3432ff8171c3b80
Adam Minkus <adam.minkus@gmail..com>
2019-07-09 22:39:54 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # friendfinder
+
+This program is a compatibility-based application. This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+## Getting Started
+
+- Clone repo
+- Run install npm to install all dependencies
+- start server port
+
+## Tech Used
+
+Node.js
+Express
+
+## Prerequisites
+
+Node.js 
+
+## Installing
+
+For application to run you will need to install all the npm packages used. You do this by running 'node install npm' in the server.js file. This should load all the packages mentioned in the package.json file
+
+Now you should be up and running!
+
+## Preview
+
+* Follow this link to see a demo of the application
+
+https://drive.google.com/file/d/1i_9vcTzG1ItdbAZHJyvGdK0Hgl1MdzoX/view
+
+##Authors
+
+* Adam Minkus - https://github.com/aminkus
+
+## Acknowledgments
+
+* Joe Rehfuss
+* Trae Shanks
+* Lan Truong Guthmann
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 var friendsArray = [
     {
-        name: "Adam",
-        photo: "https://media.istockphoto.com/photos/serious-looking-rabbi-picture-id182775759?k=6&m=182775759&s=612x612&w=0&h=hdZps71j7xNRClCMT17oLYdwMqCEFh3g5uOmHiY9VMM=",
+        name: "Marc",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkeaezoYmyX9qI6Qp7yd35UQ46PVyhMelVgIJv_mHPYW7tfmF6Ew",
         scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     },
     {
         name: "Sarah",
-        photo: "https://media.istockphoto.com/photos/serious-looking-rabbi-picture-id182775759?k=6&m=182775759&s=612x612&w=0&h=hdZps71j7xNRClCMT17oLYdwMqCEFh3g5uOmHiY9VMM=",
+        photo: "https://media.istockphoto.com/photos/beautiful-young-woman-outdoors-picture-id619275146?k=6&m=619275146&s=612x612&w=0&h=u3Bb-EwyWxsAGwLMz0DrlvgHPi92txW3o28nTB9SS2I=",
         scores: [5, 2, 2, 4, 3, 4, 2, 3, 3, 3]
     },
     {
         name: "Bob ",
-        photo: "https://media.istockphoto.com/photos/serious-looking-rabbi-picture-id182775759?k=6&m=182775759&s=612x612&w=0&h=hdZps71j7xNRClCMT17oLYdwMqCEFh3g5uOmHiY9VMM=",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMD7JALHFf-NUc4YE-IT-IQrwjYyDd80FA1-B2m-Ukg1muTTaniQ",
         scores: [5, 1, 2, 4, 3, 5, 2, 3, 3, 5]
     },
     {
@@ -21,7 +21,7 @@ var friendsArray = [
     },
     {
         name: "Lisa",
-        photo: "https://media.istockphoto.com/photos/serious-looking-rabbi-picture-id182775759?k=6&m=182775759&s=612x612&w=0&h=hdZps71j7xNRClCMT17oLYdwMqCEFh3g5uOmHiY9VMM=",
+        photo: "https://ichef.bbci.co.uk/news/660/cpsprodpb/FD27/production/_101970846_aubreyblanche.jpg",
         scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     },
 ]
